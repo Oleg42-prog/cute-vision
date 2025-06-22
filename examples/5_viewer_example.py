d1bbd7e89228f7ded7c4e92a0411fd5bc2eda70d
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-23 01:05:54 +0300

    🚸 (examples): Add basic Viewer example

---
+++ examples/5_viewer_example.py
@@ -0,0 +1,8 @@
+from cute_vision.cameras import DeviceCamera
+from cute_vision.view import Viewer
+
+viewer = Viewer('main-window')
+
+for frame in DeviceCamera(device_index=0).frames():
+    if not viewer(frame):
+        break
