97206cd6290662e0087ff572b232c7a1e26b5f58
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-23 00:10:49 +0300

    📦️ (view): Update colors __init__ routes

---
+++ cute_vision/view/draw/colors/__init__.py
@@ -0,0 +1,40 @@
+from cute_vision.view.draw.colors.bgr import BGR_RED
+from cute_vision.view.draw.colors.bgr import BGR_GREEN
+from cute_vision.view.draw.colors.bgr import BGR_BLUE
+from cute_vision.view.draw.colors.bgr import BGR_YELLOW
+from cute_vision.view.draw.colors.bgr import BGR_MAGENTA
+from cute_vision.view.draw.colors.bgr import BGR_CYAN
+from cute_vision.view.draw.colors.bgr import BGR_GRAY
+from cute_vision.view.draw.colors.bgr import BGR_WHITE
+from cute_vision.view.draw.colors.bgr import BGR_BLACK
+from cute_vision.view.draw.colors.rgb import RGB_RED
+from cute_vision.view.draw.colors.rgb import RGB_GREEN
+from cute_vision.view.draw.colors.rgb import RGB_BLUE
+from cute_vision.view.draw.colors.rgb import RGB_YELLOW
+from cute_vision.view.draw.colors.rgb import RGB_MAGENTA
+from cute_vision.view.draw.colors.rgb import RGB_CYAN
+from cute_vision.view.draw.colors.rgb import RGB_GRAY
+from cute_vision.view.draw.colors.rgb import RGB_WHITE
+from cute_vision.view.draw.colors.rgb import RGB_BLACK
+
+
+__all__ = [
+    'BGR_RED',
+    'BGR_GREEN',
+    'BGR_BLUE',
+    'BGR_YELLOW',
+    'BGR_MAGENTA',
+    'BGR_CYAN',
+    'BGR_GRAY',
+    'BGR_WHITE',
+    'BGR_BLACK',
+    'RGB_RED',
+    'RGB_GREEN',
+    'RGB_BLUE',
+    'RGB_YELLOW',
+    'RGB_MAGENTA',
+    'RGB_CYAN',
+    'RGB_GRAY',
+    'RGB_WHITE',
+    'RGB_BLACK'
+]
