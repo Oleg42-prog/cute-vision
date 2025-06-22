3f19cddab663f40fb86dbf81bcf34c5c99c398c0
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-23 01:12:42 +0300

    ✨ (cameras): Implement VideoCamera class

--- cute_vision/cameras/video_camera.py
+++ cute_vision/cameras/video_camera.py
@@ -1,5 +1,36 @@
+import cv2
+import numpy as np
 from cute_vision.cameras.abstract_camera import AbstractCamera
 
 
 class VideoCamera(AbstractCamera):
-    ...
+
+    def __init__(self, path: str, width: int = 640, height: int = 480):
+        super().__init__()
+        self.path = path
+        self.width = width
+        self.height = height
+        self.cap = None
+
+    def connect(self) -> bool:
+        self.cap = cv2.VideoCapture(self.path)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        return self.cap.isOpened()
+
+    def disconnect(self):
+        self.cap.release()
+
+    def fetch_frame(self) -> np.ndarray | None:
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    @property
+    def frame_width(self):
+        return self.width
+
+    @property
+    def frame_height(self):
+        return self.height
