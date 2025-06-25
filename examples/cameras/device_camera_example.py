f73a0a7bffd284d2097697c84f10e9390a47defa
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-25 09:56:39 +0300

    🚸 (examples): Add DeviceCamera example

---
+++ examples/cameras/device_camera_example.py
@@ -0,0 +1,9 @@
+from cute_vision.cameras import DeviceCamera
+from cute_vision.view import Viewer
+
+DEVICE_INDEX = 0
+WIDTH = 1280
+HEIGHT = 720
+
+viewer = Viewer('Device Camera Example')
+viewer.play(DeviceCamera(device_index=DEVICE_INDEX, width=WIDTH, height=HEIGHT).frames())
