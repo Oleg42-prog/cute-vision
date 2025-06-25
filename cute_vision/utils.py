fcce9d368714b0a2e177bf6db9c57c41d1aa0565
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-25 11:45:16 +0300

    ✨ (utils): Add safe_first function

--- cute_vision/utils.py
+++ cute_vision/utils.py
@@ -3,3 +3,12 @@ from typing import Any
 
 def first(iterable: Any) -> Any:
     return next(iter(iterable))
+
+
+def safe_first(iterable: Any) -> Any | None:
+    if iterable is None:
+        return None
+    try:
+        return first(iterable)
+    except StopIteration:
+        return None
