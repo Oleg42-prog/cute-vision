2c1a07bb871da7f8854b92c8853fdfa86a2e8f98
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-25 10:01:17 +0300

    🩹 (cameras): Remove width/height args from VideoCamera init because its doesnt work

--- cute_vision/cameras/video_camera.py
+++ cute_vision/cameras/video_camera.py
@@ -5,17 +5,13 @@ from cute_vision.cameras.abstract_camera import AbstractCamera
 
 class VideoCamera(AbstractCamera):
 
-    def __init__(self, path: str, width: int = 640, height: int = 480):
+    def __init__(self, path: str):
         super().__init__()
         self.path = path
-        self.width = width
-        self.height = height
         self.cap = None
 
     def connect(self) -> bool:
         self.cap = cv2.VideoCapture(self.path)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         return self.cap.isOpened()
 
     def disconnect(self):
@@ -29,8 +25,8 @@ class VideoCamera(AbstractCamera):
 
     @property
     def frame_width(self):
-        return self.width
+        raise NotImplementedError
 
     @property
     def frame_height(self):
-        return self.height
+        raise NotImplementedError
