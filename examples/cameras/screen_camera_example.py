ed2a3ceaca6ed40b846a321d5ec16fba0edd51c5
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-25 10:05:04 +0300

    🚸 (cameras): Add ScreenCamera example

---
+++ examples/cameras/screen_camera_example.py
@@ -0,0 +1,11 @@
+from cute_vision.cameras import ScreenCamera
+from cute_vision.view import Viewer
+
+MONITOR = 1
+LEFT = 0
+TOP = 0
+WIDTH = 800
+HEIGHT = 600
+
+viewer = Viewer('Screen Camera Example')
+viewer.play(ScreenCamera(monitor=MONITOR, region=(LEFT, TOP, WIDTH, HEIGHT)).frames())
