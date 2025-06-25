d336ac33f2fa48681633859ebaf7cd19052f0a61
Oleg Dudnik <Oleggelo86@gmail.com>
2025-06-25 11:54:19 +0300

    📦 (tasks): Update human_pose __init__ routes

---
+++ cute_vision/tasks/pose/__init__.py
@@ -0,0 +1,10 @@
+from cute_vision.tasks.pose.human_pose import HumanPose
+from cute_vision.tasks.pose.human_pose import HumanPosePoint
+from cute_vision.tasks.pose.human_pose import HumanPosePairPoints
+
+
+__all__ = [
+    'HumanPose',
+    'HumanPosePoint',
+    'HumanPosePairPoints'
+]
---
+++ cute_vision/tasks/pose/human_pose/__init__.py
@@ -0,0 +1,10 @@
+from cute_vision.tasks.pose.human_pose.human_pose import HumanPose
+from cute_vision.tasks.pose.human_pose.human_pose_point import HumanPosePoint
+from cute_vision.tasks.pose.human_pose.human_pose_pair_points import HumanPosePairPoints
+
+
+__all__ = [
+    'HumanPose',
+    'HumanPosePoint',
+    'HumanPosePairPoints'
+]
